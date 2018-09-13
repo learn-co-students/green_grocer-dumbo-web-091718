142773c276d9b8f36c15142a8f413b339f34aa18
christine gomez <christine.gomez1226@gmail.com>
2018-09-13 20:26:57 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,80 @@
 def consolidate_cart(cart)
-  # code here
+  result = {}
+  cart.each do |item, value|
+    item.each do |food, info|
+      if result[food]
+        result[food][:count] +=1
+      else result[food] = info
+        result[food][:count] = 1
+      end
+    end
+  end
+  result
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  result ={}
+  cart.each do |item, info|
+    coupons.each do |coupon|
+      if item == coupon[:item] && info[:count] >= coupon[:num]
+        info[:count] = info[:count] - coupon[:num]
+        if result["#{item} W/COUPON"]
+          result["#{item} W/COUPON"][:count] +=1
+        else result["#{item} W/COUPON"] = {:price => coupon[:cost],
+        :clearance => info[:clearance], :count => 1}
+      end
+    end
+  end
+  result[item] = info
+end
+result
 end
 
 def apply_clearance(cart)
-  # code here
+  clearance_cart = {}
+  cart.each do |item, info|
+    clearance_cart[item] = {}
+    if info[:clearance] == true
+      clearance_cart[item][:price] = info[:price] * 4/5
+    else clearance_cart[item][:price] = info[:price]
+    end
+    clearance_cart[item][:clearance] = info[:clearance]
+    clearance_cart[item][:count] = info[:count]
+  end
+  clearance_cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  total = 0
+  cart = consolidate_cart(cart)
+
+  if cart.length == 1
+    cart = apply_coupons(cart, coupons)
+    cart_clearance = apply_clearance(cart)
+    if cart_clearance.length > 1
+      cart_clearance.each do |item, details|
+        if details[:count] >=1
+          total += (details[:price]*details[:count])
+        end
+      end
+    else
+      cart_clearance.each do |item, details|
+        total += (details[:price]*details[:count])
+      end
+    end
+  else
+    cart = apply_coupons(cart, coupons)
+    cart_clearance = apply_clearance(cart)
+    cart_clearance.each do |item, details|
+      total += (details[:price]*details[:count])
+    end
+  end
+
+
+  if total > 100
+    total = total*(0.90)
+  end
+  total
+
+
 end
